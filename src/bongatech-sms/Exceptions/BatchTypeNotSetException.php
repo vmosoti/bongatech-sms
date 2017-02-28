706998e6331f0fc383a0aa67bb12e242726bb2e8
Vincent Mosoti <vincent@vmosoti.com>
2017-03-01 01:55:30 +0300

    populate common sms class methods

---
+++ src/bongatech-sms/Exceptions/BatchTypeNotSetException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * BongaTech SMS Client Library for PHP.
+ *
+ * @copyright Copyright (c) 2017
+ * @author   Vincent Mosoti <vincent@vmosoti.com>
+ * @license https://github.com/VMosoti/bongatech-sms/blob/master/LICENSE
+ */
+
+namespace VMosoti\BongaTech\Exceptions;
+
+
+class BatchTypeNotSetException extends \Exception
+{
+}
